238965c742536728845d00d299fec20771edbd31
xrafffcode <yusfiwawan2@gmail.com>
2023-11-10 10:22:14 +0700

    feat: competition db

---
+++ app/Models/Competition.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Competition extends Model
+{
+    use HasFactory, UUID;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'poster',
+        'guidebook',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    /**
+     * Get the teams for the competition.
+     */
+    public function teams()
+    {
+        return $this->hasMany(Team::class);
+    }
+}
