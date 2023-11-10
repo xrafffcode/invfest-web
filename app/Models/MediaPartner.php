f45bcb9ea140be93cc546bdc73d0422b1df765ca
xrafffcode <yusfiwawan2@gmail.com>
2023-11-10 12:29:04 +0700

    feat(db): media partner

---
+++ app/Models/MediaPartner.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MediaPartner extends Model
+{
+    use HasFactory, UUID;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'logo',
+        'link',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    /**
+     * Get logo attribute.
+     */
+    public function getLogoAttribute($value)
+    {
+        return asset('storage/' . $value);
+    }
+
+    /**
+     * Set logo attribute.
+     */
+    public function setLogoAttribute($value)
+    {
+        $this->attributes['logo'] = $value->store('assets/media-partners', 'public');
+    }
+}
