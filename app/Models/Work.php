cbec740026c9f42fc5c7799ae564b47967576730
xrafffcode <yusfiwawan2@gmail.com>
2023-11-10 12:30:22 +0700

    feat(db): work

---
+++ app/Models/Work.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Work extends Model
+{
+    use HasFactory, UUID;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'team_id',
+        'title',
+        'url',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'string',
+        'team_id' => 'string',
+    ];
+
+    /**
+     * Get the team that owns the work.
+     */
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
---
+++ database/migrations/2023_11_10_040821_create_works_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('team_id');
+            $table->string('title');
+            $table->string('url');
+            $table->boolean('is_reviewed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('works');
+    }
+};
