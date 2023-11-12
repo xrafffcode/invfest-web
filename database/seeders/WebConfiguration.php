a01773b22d746f67ee2d70dcdef8d7e065d9e3ff
xrafffcode <yusfiwawan2@gmail.com>
2023-11-12 23:14:07 +0700

    Add hover colors to primary and secondary colors in WebConfiguration seeder

--- database/seeders/WebConfiguration.php
+++ database/seeders/WebConfiguration.php
@@ -23,7 +23,9 @@ class WebConfiguration extends Seeder
             'email' => 'invfest@gmail.com',
             'phone' => '081234567890',
             'primary_color' => '#000000',
+            'primary_color_hover' => '#000000',
             'secondary_color' => '#000000',
+            'secondary_color_hover' => '#000000',
             'twibbon_link' => 'https://invfest.my.id',
         ]);
     }
