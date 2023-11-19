f8ad1149d7bd2e6dcba78dccaaf0c3b67a637054
xrafffcode <yusfiwawan2@gmail.com>
2023-11-19 23:11:58 +0700

    Refactor timeline.blade.php file

--- resources/views/components/frontend/timeline.blade.php
+++ resources/views/components/frontend/timeline.blade.php
@@ -1,11 +1,12 @@
 <div class="row my-5">
     <div class="col-md-12">
-       <h2 class="text-center mb-5 text-uppercase fw-bold" style="color: var(--secondary)">{{ sprintf('%s Timelines', getWebConfiguration()->title) }}</h2>
+        <h2 class="text-center mb-5 text-uppercase fw-bold" style="color: var(--secondary)">
+            {{ sprintf('%s Timelines', getWebConfiguration()->title) }}</h2>
         <div id="content_timeline">
             <ul class="timeline mx-auto">
 
-                @foreach($timelines as $timeline)
-                    <li class="card event" data-date="{{ date('Y-m-d', strtotime($timeline->date)) }}">
+                @foreach ($timelines as $timeline)
+                    <li class="card event" data-date="{{ date('d-m-Y', strtotime($timeline->date)) }}">
                         <div class="wrap_info">
                             <h3>{{ $timeline->title }}</h3>
                             <p>{{ $timeline->description }}</p>
