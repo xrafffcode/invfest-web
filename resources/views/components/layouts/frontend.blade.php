bb7325274ec2cc6c5086ee07cf0f4ddea490c5aa
xrafffcode <yusfiwawan2@gmail.com>
2023-11-20 14:44:27 +0700

    Update og:image and twitter:image URLs

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -17,16 +17,14 @@
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ $title }}">
     <meta property="og:description" content="{{ $description ?? '' }}">
-    <meta property="og:image" content="https://i.ytimg.com/vi/kX0tq3qsY_U/maxresdefault.jpg">
-
+    <meta property="og:image" content="{{ asset('opengraphimg.jpg') }}">
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
     <meta property="twitter:domain" content="{{ url()->current() }}">
     <meta property="twitter:url" content="{{ url()->current() }}">
     <meta name="twitter:title" content="{{ $title }}">
     <meta name="twitter:description" content="{{ $description ?? '' }}">
-    <meta name="twitter:image" content="https://i.ytimg.com/vi/kX0tq3qsY_U/maxresdefault.jpg">
-
+    <meta name="twitter:image" content="{{ asset('opengraphimg.jpg') }}">
 
     <link rel="shortcut icon" href="{{ asset(getWebConfiguration()->nav_logo) }}" type="image/x-icon">
 
